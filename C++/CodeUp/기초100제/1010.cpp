a026182a7e5cd78d6801a4694857a5e79700d42e
JMyeongJun <zcym002@naver.com>
2022-01-08 15:42:38 +0900

    Add 기초100제 풀이

---
+++ C++/CodeUp/기초100제/1010.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// #pragma warning(disable:4996)
+// #define _CRT_SECURE_NO_WARNINGS	//scanf 보안문제 해결
+
+int main() {
+	int a;
+	scanf("%d", &a);
+	printf("%d", a);
+	return 0;
+}
