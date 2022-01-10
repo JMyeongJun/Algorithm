f12ce1cdd5b9d6b06d7a738a33729e06f5bfa0b1
JMyeongJun <zcym002@naver.com>
2022-01-10 22:33:59 +0900

    Add solved problems

---
+++ Java/Baekjoon/Step_By_Step/src/step_1/No_1008.java
@@ -0,0 +1,16 @@
+package step_1;
+
+import java.util.Scanner;
+
+public class No_1008 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+
+		a = sc.nextInt();
+		b = sc.nextInt();
+
+		System.out.println(a / (double)b);
+		sc.close();
+	}
+}
