66b84b64af9e349e86e026631902ec1828618595
JMyeongJun <zcym002@naver.com>
2022-01-15 01:54:38 +0900

    Add solved problems

---
+++ Java/Baekjoon/Step_By_Step/src/step3_for/No_11021.java
@@ -0,0 +1,20 @@
+package step3_for;
+
+import java.util.Scanner;
+
+public class No_11021 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+
+		for (int i = 1; i <= t; i++) {
+			int a, b;
+			a = sc.nextInt();
+			b = sc.nextInt();
+			
+			System.out.println("Case #" + i + ": " + (a+b));
+		}
+		
+		sc.close();
+	}
+}
