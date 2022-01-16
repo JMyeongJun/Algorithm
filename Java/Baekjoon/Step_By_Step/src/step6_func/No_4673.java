558a985444c57f4165e0daa1f79eaeba74c47ec2
JMyeongJun <zcym002@naver.com>
2022-01-16 23:43:22 +0900

    Add solved problems

--- Java/Baekjoon/Step_By_Step/src/step6_func/No_4673.java
+++ Java/Baekjoon/Step_By_Step/src/step6_func/No_4673.java
@@ -1,5 +1,33 @@
 package step6_func;
 
 public class No_4673 {
-	
+	static int d(int num) {
+		int ret = num;
+
+		while (num != 0) {
+			ret += num % 10;
+			num /= 10;
+		}
+
+		return ret;
+	}
+
+	public static void main(String[] args) {
+				
+		for(int i = 1; i<10000; i++) {
+			
+			boolean chk = false;
+			
+			for(int j = 1; j<10000; j++) {
+				if(i == d(j)) {
+					chk = true;
+					break;
+				}
+			}
+			
+			if(!chk) {
+				System.out.println(i);
+			}
+		}
+	}
 }
