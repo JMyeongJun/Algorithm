91e7e76f6189d6ae014c5e6a0dfda269dcdaee58
JMyeongJun <zcym002@naver.com>
2022-01-17 00:39:08 +0900

    Add solved problems

---
+++ Java/Baekjoon/Step_By_Step/src/step6_func/No_1065.java
@@ -0,0 +1,44 @@
+package step6_func;
+
+import java.util.Scanner;
+
+public class No_1065 {
+
+	public static boolean hansu(int num) {
+		String n = Integer.toString(num);
+		int diff1, diff2;
+
+		if (num < 100) {
+			return true;
+		}
+
+		for (int i = 0; i < n.length() - 2; i++) {
+
+			diff1 = Integer.parseInt(String.valueOf(n.charAt(i))) 
+					- Integer.parseInt(String.valueOf(n.charAt(i + 1)));
+			diff2 = Integer.parseInt(String.valueOf(n.charAt(i + 1)))
+					- Integer.parseInt(String.valueOf(n.charAt(i + 2)));
+
+			if (diff1 != diff2) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(), cnt = 0;
+
+		for (int i = 1; i <= n; i++) {
+			if (hansu(i)) {
+				cnt++;
+			}
+		}
+
+		System.out.println(cnt);
+
+		sc.close();
+	}
+}
