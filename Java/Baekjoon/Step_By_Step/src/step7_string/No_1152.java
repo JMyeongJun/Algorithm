05bffce23b78dfaacce28abe458c43daeb4dd9d5
JMyeongJun <zcym002@naver.com>
2022-01-18 00:06:02 +0900

    Improve performance of No_1152.java

--- Java/Baekjoon/Step_By_Step/src/step7_string/No_1152.java
+++ Java/Baekjoon/Step_By_Step/src/step7_string/No_1152.java
@@ -1,25 +1,15 @@
 package step7_string;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class No_1152 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		int cnt = 0;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		for(int i =1; i< str.length()-1; i++) {
-			if(str.charAt(i) == ' ') {
-				cnt++;
-			}
-		}
-		
-		if(str.trim().equals("")) {
-			System.out.println(cnt);
-		}else {
-			System.out.println(cnt+1);
-		}
-		
-		sc.close();
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		System.out.println(st.countTokens());
 	}
 }
