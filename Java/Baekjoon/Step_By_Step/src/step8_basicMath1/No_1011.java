56d7fa291055591c6928c8af297f7090b1d58777
JMyeongJun <zcym002@naver.com>
2022-01-28 01:15:54 +0900

    Simplify No_1011.java

--- Java/Baekjoon/Step_By_Step/src/step8_basicMath1/No_1011.java
+++ Java/Baekjoon/Step_By_Step/src/step8_basicMath1/No_1011.java
@@ -10,34 +10,26 @@ public class No_1011 {
 		return num * (num +1) / 2;
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
 		
 		for (int i = 0; i < t; i++) {
 			int x = sc.nextInt(), y = sc.nextInt(), k = 1;
 			int dist = y - x, cnt = 0;
-//			System.out.println("dist = " + dist);
-//			System.out.println("---------------");
+			System.out.println("dist : " + dist);
 			
 			while(dist != 0) {
 				dist -= k;
-//				System.out.println("dist - k = " + dist);
+				cnt++;
 				
 				if(dist >= sum(k+1)) {
-//					System.out.println("k++");
 					k++;
 				}else if(dist < sum(k)) {
-//					System.out.println("k--");
-//					System.out.println(cnt);
 					k--;
-				}else {
-//					System.out.println("k+0");
 				}
-//				System.out.println("k = " + k);
-				cnt++;
-//				System.out.println("---------------");
 			}
+			
 			System.out.println(cnt);
 		}
 		
