d143633550679d71b024f6b52ccb2edfe637f61b
JoMeongJun <zcym002@naver.com>
2022-01-27 18:33:50 +0900

    Add solved problems

---
+++ Java/Baekjoon/Step_By_Step/src/step1_InputOutput/No_10926.java
@@ -0,0 +1,13 @@
+package step1_InputOutput;
+
+import java.util.Scanner;
+
+public class No_10926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String id = sc.next() + "??!";
+		sc.close();
+		
+		System.out.println(id);
+	}
+}
---
+++ Java/Baekjoon/Step_By_Step/src/step1_InputOutput/No_18108.java
@@ -0,0 +1,13 @@
+package step1_InputOutput;
+
+import java.util.Scanner;
+
+public class No_18108 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int y = sc.nextInt() - 543;
+		sc.close();
+		
+		System.out.println(y);
+	}
+}
