e3ae5fcd045ee64613c153a74a76db38d98ce2f1
JoMeongJun <zcym002@naver.com>
2022-02-14 14:43:15 +0900

    Update and rename Main.java to No_1978.java

--- Java/Baekjoon/Step_By_Step/src/step9_basicMath2/No_1978.java
+++ Java/Baekjoon/Step_By_Step/src/step9_basicMath2/No_1978.java
@@ -1,8 +1,8 @@
-package ex01;
+package step9_basicMath2;
 
 import java.util.Scanner;
 
-public class Main {
+public class No_1978 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
