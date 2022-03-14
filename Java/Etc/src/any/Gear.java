5bcf25dcdca3bafe43547d513f7567b59e685f2d
JMyeongJun <zcym002@naver.com>
2022-03-14 09:15:50 +0900

    refactor gear

--- Java/Etc/src/any/Gear.java
+++ Java/Etc/src/any/Gear.java
@@ -7,16 +7,35 @@ public class Gear {
 //	1 0 0 0 0 1 0 0 1 1 0 0 톱니2
 //	0 0 1 0 0 0 0 0 0 0 0 0 톱니3
 	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
 
-		int[][] gear = {
-				{ 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0 }, 
-				{ 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0 },
-				{ 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1 } };
+//		int[][] gear = {
+//				{ 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0 }, 
+//				{ 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0 },
+//				{ 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1 } };
+		
+		
 
+		// debuging code
 //		System.out.println(getOneIndex(gear));
+		
+		// 최종 출력
+		for(int i = 0; i < 10000; i++) {
+			calc();
+		}
+		
+		System.out.println(System.currentTimeMillis() - start);
+	}
+	
+	public static void calc() {
+		int[][] gear = {
+				{ 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+				{ 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0 },
+				{ 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0 } };
 		System.out.println(getMinPath(getOneIndex(gear)));
 	}
 
+	// '1'인 인덱스 값을 이중 ArrayList로 return
 	public static ArrayList<ArrayList<Integer>> getOneIndex(int[][] arr) {
 		ArrayList<ArrayList<Integer>> indexList = new ArrayList<ArrayList<Integer>>();
 		
@@ -31,9 +50,11 @@ public class Gear {
 			
 			indexList.add(index);
 		}
+		
 		return indexList;
 	}
 	
+	// getOneIndex 반환값을 매개변수로 최소 거리 계산 후 거리 반환
 	public static int getMinPath(ArrayList<ArrayList<Integer>> indexList) {
 		ArrayList<Integer> temp;
 		int size = indexList.size(); // == 3
@@ -41,13 +62,13 @@ public class Gear {
 		
 		for(int i = 0; i < size; i++) {
 			temp = indexList.remove(i);
+			int tempsize = temp.size();
+			int listsize0 = indexList.get(0).size();
+			int listsize1 = indexList.get(1).size();
 			
-			for(int j = 0; j < temp.size(); j++) {
-				
-				for(int k = 0; k < indexList.get(0).size(); k++) {
-					
-					for(int z = 0; z < indexList.get(1).size(); z++) {
-						
+			for(int j = 0; j < tempsize; j++) {
+				for(int k = 0; k < listsize0; k++) {
+					for(int z = 0; z < listsize1; z++) {
 						int path = 
 						getPath(temp.get(j), indexList.get(0).get(k)) +
 						getPath(temp.get(j), indexList.get(1).get(z));
@@ -55,13 +76,11 @@ public class Gear {
 						if(path < minPath) {
 							minPath = path;
 						}
-						
 					}
-					
 				}
-				
 			}
 			
+			// debuging code
 //			System.out.println(temp);
 //			System.out.println(indexList);
 			indexList.add(i, temp);
@@ -70,6 +89,7 @@ public class Gear {
 		return minPath;
 	}
 	
+	// 배열 인덱스 사이 거리 계산
 	public static int getPath(int idx1, int idx2) {
 		int ret = Math.abs(idx1 - idx2);
 		if(ret > 12/2) {
