143854a9e380e5c92cad6cdfdc0a0c2afeac15a1
JMyeongJun <zcym002@naver.com>
2022-03-29 23:44:21 +0900

    문제 11653 : 소인수분해

---
+++ Java/Baekjoon/Step_By_Step/src/step9_basicMath2/No_11653.java
@@ -0,0 +1,24 @@
+package step9_basicMath2;
+
+import java.util.Scanner;
+
+// 정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
+
+public class No_11653 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		
+		for(int i = 2; n != 1; i++) {
+			if(n % i == 0) {
+				n /= i;
+				System.out.println(i);
+				i = 1;
+			}
+		}
+	}
+	
+}
