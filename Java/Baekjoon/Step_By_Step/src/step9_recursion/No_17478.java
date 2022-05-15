2371f4a7d823ac1cb0e3cb29edd607ab9a04c6a9
JMyeongJun <zcym002@naver.com>
2022-05-15 22:05:51 +0900

    문제 17478 : 재귀(성능 개선)

--- Java/Baekjoon/Step_By_Step/src/step9_recursion/No_17478.java
+++ Java/Baekjoon/Step_By_Step/src/step9_recursion/No_17478.java
@@ -1,26 +1,38 @@
 package step9_recursion;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class No_17478 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
-		recursion("", n);
-		sc.close();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		bw.write("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+		bw.write(recursion("", n));
+		
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 	
-	public static void recursion(String underline, int num) {
-		System.out.println(underline + "\"재귀함수가 뭔가요?\"");
+	public static String recursion(String underline, int num) {
+		String ret = "";
+		ret = underline + "\"재귀함수가 뭔가요?\"\n";
 		if(num >= 1) {
-			System.out.println(underline + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
-			System.out.println(underline + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
-			System.out.println(underline + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
-			recursion(underline + "____", num - 1);
+			ret += underline + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n";
+			ret += underline + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n";
+			ret += underline + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n";
+			ret += recursion(underline + "____", num - 1);
 		}else {
-			System.out.println(underline + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
+			ret += underline + "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n";
 		}
-		System.out.println(underline + "라고 답변하였지.");
+		ret += underline + "라고 답변하였지.\n";
+		
+		return ret;
 	}
 }
